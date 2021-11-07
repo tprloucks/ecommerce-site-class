cfd4dfb0911d8b96ab4eacea8ab4e0567d6c99cc
Jose Bosquez <josea_bosquez@outlook.com>
2021-11-06 19:00:41 -0600

    created and exported our products

---
+++ src/components/ProductsList.js
@@ -0,0 +1,30 @@
+const allProducts = [
+    {
+        id: '1',
+        name: "red boat",
+        description: 'a red boat',
+        brand: 'Bertram',
+        price: 1000,
+        image: 'https://cdn.trendhunterstatic.com/thumbs/speedboat.jpeg'
+    }
+    ,
+    {
+        id: '2',
+        name: "blue boat",
+        description: 'a blue boat',
+        brand: 'Boston Whaler',
+        price: 1000,
+        image: 'https://thecoolhunter.net/wp-content/uploads/old-blog-images/blueb.jpg'
+    }
+]
+
+export const fetchProducts = () => new Promise ((resolve, reject) => {
+    console.log("fetch the products")
+    setTimeout(()=> {
+        try {
+            resolve(allProducts)
+        } catch (e) {
+            reject(e)
+        }
+    }, 1000)
+})
